4ce3d8862603197a1518d086b0c8bdd6cdf3057f
Anuraj Gogoi <anurajgogoi01@gmail.com>
2025-06-28 01:58:29 +0530

    cor middleware removed

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,6 @@ app.use(
 );
 app.use(express.json());
 
-// Example for Node.js with Express
-app.use((req, res, next) => {
-  // Set the Access-Control-Allow-Origin header to the origin of your frontend
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-  // You might also need to allow specific headers and methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
 
 //routes
 app.get("/", (req, res) => res.send("API is working"));
